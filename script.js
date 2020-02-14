09c455608499f97a1ff01a9764d6f3d0da9621ab
Omar Caraballo <21061@columbushs.com>
2020-02-14 13:44:56 -0500

    buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+let rules = document.getElementById('rulesButton');
+
+let play = document.getElementById('playButton');
+
+let back = document.getElementById('backButton');
+
+let titleScreen = document.getElementById('title-screen');
+
+let rulesScreen = document.getElementById('rules-screen');
+
