a13e3816844c65fd82f14b08f2634a0d436c3a60
Omar Caraballo <21061@columbushs.com>
2020-02-18 09:41:28 -0500

    colors

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let rules = document.getElementById('rulesButton');
+let rules = document.getElementById("rulesButton")
 
-let play = document.getElementById('playButton');
+let play = document.getElementById("playButton");
 
-let back = document.getElementById('backButton');
+let back = document.getElementById("backButton");
 
-let titleScreen = document.getElementById('title-screen');
+let titleScreen = document.getElementById("titleScreen");
+let rulesScreen = document.getElementById("rulesScreen");
+let playScreen = document.getElementById("playScreen")
 
-let rulesScreen = document.getElementById('rules-screen');
-
+rules.onclick = () => { 
+    titleScreen.style.visibility = 'hidden';
+    rulesScreen.style.visibility = 'visible';
+}
+ back.onclick = () => {
+    titleScreen.style.visibility = 'visible';
+    rulesScreen.style.visibility = 'hidden';
+ }
+ play.onclick = () => {
+     playScreen.style.visibility = 'visible';
+     titleScreen.style.visibility = 'hidden';
+ }
