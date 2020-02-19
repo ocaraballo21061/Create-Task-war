200c96afe8c768f1dec704209159530f6851918a
Omar Caraballo <21061@columbushs.com>
2020-02-19 12:56:31 -0500

    Game

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ let play = document.getElementById("playButton");
 
 let back = document.getElementById("backButton");
 
+let blueSelect = document.getElementById('blueSelect');
+
+let redSelect = document.getElementById('redSelect');
+
+let blue = true;
+
 let titleScreen = document.getElementById("titleScreen");
 let rulesScreen = document.getElementById("rulesScreen");
-let playScreen = document.getElementById("playScreen")
+let selectScreen = document.getElementById("selectScreen");
+let gameScreen = document.getElementById("gameScreen");
 
-rules.onclick = () => { 
+rules.onclick = () => {
     titleScreen.style.visibility = 'hidden';
     rulesScreen.style.visibility = 'visible';
 }
- back.onclick = () => {
+back.onclick = () => {
     titleScreen.style.visibility = 'visible';
     rulesScreen.style.visibility = 'hidden';
- }
- play.onclick = () => {
-     playScreen.style.visibility = 'visible';
-     titleScreen.style.visibility = 'hidden';
- }
+}
+play.onclick = () => {
+    selectScreen.style.visibility = 'visible';
+    titleScreen.style.visibility = 'hidden';
+}
+
+blueSelect.onclick = () => {
+    selectScreen.style.visibility = 'hidden';
+    gameScreen.style.visibility = 'visible';
+}
+
+redSelect.onclick = () => {
+    selectScreen.style.visibility = 'hidden';
+    gameScreen.style.visibility = 'visible';
+}
