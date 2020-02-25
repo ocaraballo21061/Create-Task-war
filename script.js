bc905218aa728574febabe195c2fe98cd3c5464a
Omar Caraballo <ocaraballo21061@columbushs.com>
2020-02-24 21:55:39 -0500

    JAVASCRIPT

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ let p1deck = document.getElementById('p1deck');
 
 let p2deck = document.getElementById('p2deck')
 
+let p1slot = document.getElementById('p1slot');
+
+let p2slot = document.getElementById('p2slot');
+
+let p1num;
+
+let p2num;
+
 let blue = true;
 
 let titleScreen = document.getElementById("titleScreen");
@@ -45,4 +53,104 @@ redSelect.onclick = () => {
     gameScreen.style.visibility = 'visible';
     p1deck.style.backgroundImage =  "url('images/red_back.png')";
     p2deck.style.backgroundImage =  "url('images/blue_back.png')";
+    blue = !blue;
 }
+
+
+
+
+function generateRandomCard(){
+ p1num = Math.ceil(Math.random() * 13 + 1);
+
+ p2num = Math.ceil(Math.random() * 13 + 1);
+}
+
+function player1Card(){
+    if (p1num == 2){
+        p1slot.style.backgroundImage = "url('images/2S.png')";
+    }
+    else if (p1num == 3){
+        p1slot.style.backgroundImage = "url('images/3S.png')";
+    }
+    else if (p1num == 4){
+        p1slot.style.backgroundImage = "url('images/4S.png')";
+    }
+    else if (p1num == 5){
+        p1slot.style.backgroundImage = "url('images/5S.png')";
+    }
+    else if (p1num == 6){
+        p1slot.style.backgroundImage = "url('images/6S.png')";
+    }
+    else if (p1num == 7){
+        p1slot.style.backgroundImage = "url('images/7S.png')";
+    }
+    else if (p1num == 8){
+        p1slot.style.backgroundImage = "url('images/8S.png')";
+    }
+    else if (p1num == 9){
+        p1slot.style.backgroundImage = "url('images/9S.png')";
+    }
+    else if (p1num == 10){
+        p1slot.style.backgroundImage = "url('images/10S.png')";;
+    }
+    else if (p1num == 11){
+        p1slot.style.backgroundImage = "url('images/JS.png')";
+    }
+    else if (p1num == 2){
+        p1slot.style.backgroundImage = "url('images/QS.png')";
+    }
+    else if (p1num == 13){
+        p1slot.style.backgroundImage = "url('images/KS.png')";
+    }
+    else if (p1num == 14){
+        p1slot.style.backgroundImage = "url('images/AS.png')";
+    }
+    
+}
+ function player2Card(){
+     if (p2num == 2){
+        p2slot.style.backgroundImage = "url('images/2H.png')";
+    }
+    else if (p2num == 3){
+        p2slot.style.backgroundImage = "url('images/3H.png')";
+    }
+    else if (p2num == 4){
+        p2slot.style.backgroundImage = "url('images/4H.png')";
+    }
+    else if (p2num == 5){
+        p2slot.style.backgroundImage = "url('images/5H.png')";
+    }
+    else if (p2num == 6){
+        p2slot.style.backgroundImage = "url('images/6H.png')";
+    }
+    else if (p2num == 7){
+        p2slot.style.backgroundImage = "url('images/7H.png')";
+    }
+    else if (p2num == 8){
+        p2slot.style.backgroundImage = "url('images/8H.png')";
+    }
+    else if (p2num == 9){
+        p2slot.style.backgroundImage = "url('images/9H.png')";
+    }
+    else if (p2num == 10){
+        p2slot.style.backgroundImage = "url('images/10H.png')";;
+    }
+    else if (p2num == 11){
+        p2slot.style.backgroundImage = "url('images/JH.png')";
+    }
+    else if (p2num == 12){
+        p2slot.style.backgroundImage = "url('images/QH.png')";
+    }
+    else if  (p2num == 13) {
+        p2slot.style.backgroundImage = "url('images/KH.png')";
+    }
+    else if  (p2num == 14) {
+        p2slot.style.backgroundImage = "url('images/AH.png')";
+    }
+ }
+
+generateRandomCard();
+
+player1Card();
+
+player2Card();
